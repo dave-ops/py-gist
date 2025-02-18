94d5d3c494cce1403b409a64251aa0c142ad5e3e
dave-ops <dave-ops@codeforge.cc>
2025-02-18 17:03:36 -0600

    fix linters in setup

--- setup.py
+++ setup.py
@@ -1,19 +1,35 @@
+"""
+Setup script for the py-gist project, which provides functionality to upload files to GitHub Gist.
+
+This script configures the package for distribution, defining metadata like name, version, 
+dependencies, and entry points for command-line usage.
+"""
+
 from setuptools import setup, find_packages
 
 setup(
     name='py-gist',
+    # The name of the package as it will appear on PyPI.
     version='0.1',
+    # The version of the package.
     packages=find_packages(include=['src', 'src.*']),
+    # Finds all packages under the 'src' directory to include in the distribution.
     install_requires=[
         'requests',
     ],
+    # Lists the dependencies required for this package to run. Here, 'requests' is needed for making HTTP requests.
     entry_points={
         'console_scripts': [
             'py-gist=src.main:main',
         ],
     },
+    # Defines entry points for command-line scripts. 'py-gist' command will run the main function from src.main.
     author='Your Name',
-    description='Upload files to GitHub Gist',
+    # The author of the package.
+    description='Upload files to Github Gist',
+    # A brief description of what the package does.
     long_description=open('docs/README.md').read(),
+    # Reads the content of README.md for a more detailed description of the package.
     long_description_content_type="text/markdown",
+    # Specifies that the long description is in Markdown format.
 )
