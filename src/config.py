daf9d8c3cbe4bf4f6001c2d41e78c39ac82cc2cf
dave-ops <dave-ops@codeforge.cc>
2025-02-18 14:55:32 -0600

    fix config to save os env entries

---
+++ src/config.py
@@ -0,0 +1,23 @@
+# config.py
+import os
+
+# Default values
+SOURCE_DIR_DEFAULT = 'src'
+OUTPUT_DIR_DEFAULT = 'output'
+PROJECT_NAME_DEFAULT = 'py-gist'
+
+# Gist settings
+PROJECT_NAME_DEFAULT = 'py-gist'
+
+# Ignore folders
+IGNORE_FOLDERS = ['__pycache__']
+
+# Environment variable names
+ENV_VAR_SOURCE_DIR = 'SOURCE_DIR'
+ENV_VAR_OUTPUT_DIR = 'OUTPUT_DIR'
+ENV_VAR_PROJECT_NAME = 'PROJECT_NAME'
+ENV_VAR_GITHUB_TOKEN = 'GITHUB_TOKEN'
+
+# Function to get environment variable or default value
+def get_env_or_default(env_var, default_value):
+    return os.environ.get(env_var, default_value)
--- src/main.py
+++ src/main.py
@@ -3,9 +3,15 @@ import shutil
 import time
 from utils import sanitize_filename, make_content_json_safe
 from api import create_gist, check_api_connection, check_rate_limit
-from constants import (SOURCE_DIR_DEFAULT, OUTPUT_DIR_DEFAULT, PROJECT_NAME_DEFAULT,
-                       IGNORE_FOLDERS, ENV_VAR_SOURCE_DIR, ENV_VAR_OUTPUT_DIR,
-                       ENV_VAR_PROJECT_NAME, ENV_VAR_GITHUB_TOKEN)
+from config import (SOURCE_DIR_DEFAULT, 
+                    OUTPUT_DIR_DEFAULT, 
+                    PROJECT_NAME_DEFAULT,
+                    IGNORE_FOLDERS, 
+                    ENV_VAR_SOURCE_DIR, 
+                    ENV_VAR_OUTPUT_DIR,
+                    ENV_VAR_PROJECT_NAME, 
+                    ENV_VAR_GITHUB_TOKEN
+                    )
 
 def flatten_and_upload_to_gist(folder_path, output_folder, gist_description, github_token):
     if not os.path.exists(output_folder):
